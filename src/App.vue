5c24c10292a0d66114d25f88b85173202fe7b746
XiaoDaiGua-Ray <443547225@qq.com>
2023-09-05 16:23:23 +0800

    deleted: deleted some file

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { Vue3NextQrcode } from '../es/index'
-import LOGO from './assets/vue.svg'
-</script>
-
-<template>
-  <Vue3NextQrcode text="hello" />
-  <Vue3NextQrcode text="hello" :logoImage="LOGO" />
-</template>
